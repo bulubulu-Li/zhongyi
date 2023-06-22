8afa1ac0efc3521f28fdc0451ef8bfac2dabb9ce
wangzihan14 <wangzihan14@meituan.com>
2023-06-22 11:28:43 +0800

    第六次提交

--- main.py
+++ main.py
@@ -588,15 +588,15 @@ def return_message():
                 # content = "可以"
                 query = send_message
                 for key in API_KEY:
-                    # try:
-                    chain_type_kwargs = {"prompt": PROMPT}
-                    chain = RetrievalQA.from_chain_type(llm=OpenAI(model_name="gpt-3.5-turbo-16k-0613",max_tokens=500,temperature=0,openai_api_key=key), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={'k':3}), chain_type_kwargs=chain_type_kwargs,verbose=True,return_source_documents=True)
-                    content = chain({"query":query})
-                    print(content)
-                    break
-                    # except:
-                    #     print('当前key失效，将使用新的key')
-                    #     continue
+                    try:
+                        chain_type_kwargs = {"prompt": PROMPT}
+                        chain = RetrievalQA.from_chain_type(llm=OpenAI(model_name="gpt-3.5-turbo-16k-0613",max_tokens=500,temperature=0,openai_api_key=key), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={'k':3}), chain_type_kwargs=chain_type_kwargs,verbose=True,return_source_documents=True)
+                        content = chain({"query":query})
+                        print(content)
+                        break
+                    except:
+                        print('当前key失效，将使用新的key')
+                        continue
                 # try:                
                 result = content['result']
                 a = result.split("。")
@@ -627,10 +627,11 @@ def return_message():
                     result += ('\n\n'+ "[{}] ".format(str(i+1)) + str(content["source_documents"][i].metadata))
                 # except:
                 #     result='未找到答案'
+                result += '\n\n\n召回文档：'
+                for i in source:
+                    result += ('\n\n'+ "[{}] ".format(str(i+1)) + str(content["source_documents"][i].page_content))
 
                 print(f"用户({session.get('user_id')})得到的回复消息:{result[:40]}...")
-
-                result += "\n\n 召回文档：\n\n"+'\n'.join(content["source_documents"])
 
                 if chat_with_history:
                     user_info['chats'][chat_id]['have_chat_context'] += 1
